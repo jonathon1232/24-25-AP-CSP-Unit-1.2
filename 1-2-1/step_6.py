c64198db3e73870b941c81e5eb2df1eb07225265
22520 <jonathon.frisco@vtsd.com>
2024-10-29 13:32:30 -0400

    spot guy

--- 1-2-1/step_6.py
+++ 1-2-1/step_6.py
@@ -4,7 +4,7 @@ import turtle as trtl
 
 
 #-----game configuration----
-spot_color = ("pink")
+spot_color = "pink"
 spot_size = 2
 spot_shape = "circle"
 
@@ -15,9 +15,11 @@ spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
 #-----game functions--------
+def spot_clicked(x, y):
+    print("Hello world!")
 
 
 #-----events------------
-
+spot.onclick(spot_clicked)
 wn = trtl.Screen()
 wn.mainloop()
