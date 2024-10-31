1c3af4a25a3dd2fde27d4f6dd0536a876e2f63d5
22520 <jonathon.frisco@vtsd.com>
2024-10-31 11:33:40 -0400

    Spot project completed

--- 1-2-1/step_6.py
+++ 1-2-1/step_6.py
@@ -8,20 +8,32 @@ spot_color = "pink"
 spot_size = 2
 spot_shape = "circle"
 score = 0
-
+font_setup = ("Arial", 20, "normal")
 #-----initialize turtle-----
 spot = trtl.Turtle()
 spot.shape(spot_shape)
 spot.shapesize(spot_size)
 spot.fillcolor(spot_color)
 score_writer = trtl.Turtle()
+counter =  trtl.Turtle()
 
+font_setup = ("Arial", 20, "normal")
+timer = 5
+counter_interval = 1000   #1000 represents 1 second
+timer_up = False
 #-----game functions--------
 score_writer.penup()
-score_writer.goto(0,200)
+score_writer.goto(0,300)
+counter.penup()
+counter.goto(300,300)
 def spot_clicked(x, y):
-   change_position(x, y)
-   update_score()
+    global timer_up
+    if timer_up == False:
+        update_score()
+        change_position(x, y)
+    else:
+        counter.penup()
+
 def change_position(x, y):
     new_xpos = rand.randint(-200, 200)
     new_ypos = rand.randint(-150, 150)
@@ -31,11 +43,24 @@ def change_position(x, y):
 def update_score():
     global score
     score += 1
+    score_writer.clear()
     score_writer.write(score, font=font_setup)
+def countdown():
+  global timer, timer_up
+  counter.clear()
+  if timer <= 0:
+    counter.write("Time's Up", font=font_setup)
+    timer_up = True
+  else:
+    counter.write("Timer: " + str(timer), font=font_setup)
+    timer -= 1
+    counter.getscreen().ontimer(countdown, counter_interval)
+
 
 
 
 #-----events------------
 spot.onclick(spot_clicked)
 wn = trtl.Screen()
+wn.ontimer(countdown, counter_interval)
 wn.mainloop()
