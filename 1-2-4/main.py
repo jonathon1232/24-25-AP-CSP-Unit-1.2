2afd552532b42e7c29c1353fd84d20734db708d0
22520 <jonathon.frisco@vtsd.com>
2024-12-09 12:28:58 -0500

    making snake

--- 1-2-4/main.py
+++ 1-2-4/main.py
@@ -1,41 +1,46 @@
 import turtle as trtl
 import random as rand
-from typing import Tuple
 
-# initialize variable
-
-wn=trtl.Screen()
+# Intialize Variables
+wn = trtl.Screen()
 maze_painter = trtl.Turtle()
-global length
-length = 35
 path_width = 30
+
+
+# Setup Turtle
 maze_painter.left(90)
 maze_painter.pensize(5)
-maze_painter.speed(10)
-door = rand.randint(path_width*2, (wall_len - path_width*2))
-barrier = rand.randint(path_width*2, (wall_len - path_width*2))
-barrier_limits = rand.randint(-100, 100)
-# setup turtle
+maze_painter.speed(0)
 
-def barriers():
-    global length
-    if wall > 5:
-        draw_barriers()
-    maze_painter.forward(length - path_width - (length / 3))
-    maze_painter.left(90)
-    length += 15
-
-def draw_barriers():
+# Draw Maze
+# Process:
+# Draw a line
+# Turn Left
+# Increment Length
+# Repeat
+def draw_barrier():
     maze_painter.right(90)
     maze_painter.forward(path_width)
     maze_painter.backward(path_width)
     maze_painter.left(90)
+
+length = 35
 for wall in range(21):
-    maze_painter.forward(length/3)
+    gap_space = rand.randint(0, length - path_width)
+    door_space = 0
+    maze_painter.forward(gap_space)
     maze_painter.penup()
     maze_painter.forward(path_width)
     maze_painter.pendown()
-    barriers(barrier_limits)
+    if(wall > 5):
+       door_space = rand.randint(0, length - path_width - gap_space)
+       maze_painter.forward(door_space)
+       draw_barrier()
+    maze_painter.forward(length - gap_space - path_width - door_space)
+    maze_painter.left(90)
+    length += 15
 
+
+
+wn.listen()
 wn.mainloop()
-
